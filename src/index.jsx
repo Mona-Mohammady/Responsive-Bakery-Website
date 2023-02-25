70d50e4f651e9c04d63eb757823eced647421a8a
Mona-Mohammady <119614879+Mona-Mohammady@users.noreply.github.com>
2023-02-25 15:00:20 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,15 @@ import App from './App';
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
+   <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
 
 
